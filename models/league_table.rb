dae5fe9f1de99004dc8b78b7137206b91b55d85c
Petr Balaban <balaban.petr@gmail.com>
2015-03-15 22:59:17 +0200

    [Specs passed] - added method definitions with stubs.

--- models/league_table.rb
+++ models/league_table.rb
@@ -1,2 +1,35 @@
 class LeagueTable
+  attr_accessor :matches
+
+  def matches
+    @matches ||= []
+  end
+
+  def get_points(team_name)
+    0
+  end
+
+  def get_goals_for(team_name)
+    0
+  end
+
+  def get_goals_against(team_name)
+    0
+  end
+
+  def get_goal_difference(team_name)
+    0
+  end
+
+  def get_wins(team_name)
+    0
+  end
+
+  def get_draws(team_name)
+    0
+  end
+
+  def get_losses(team_name)
+    0
+  end
 end
